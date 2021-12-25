6ffda0a543d9109451946c7916ddb0133d9514f5
Philip Tzou <philip.npc@gmail.com>
2021-12-24 21:13:58 -0800

    Update type hints for save_json.py

--- postalign/processors/save_json.py
+++ postalign/processors/save_json.py
@@ -2,31 +2,41 @@ import json
 # import math
 import click
 from textwrap import indent
+from typing import Tuple, List, Iterable, TypedDict, Optional
 from more_itertools import chunked
 
 from ..cli import cli
 from ..utils import group_by_gene_codons
 from ..utils.codonutils import translate_codon
+from ..models import RefSeqPair, NAPosition, Sequence
 
 from .trim_by_ref import find_trim_slice
+from .base import Processor, output_processor
 
 
-def group_gene_range_tuples(gene_range_tuples):
-    tuples = []
-    cur_group = []
+def gene_range_tuples_callback(
+    ctx: click.Context,
+    param: str,
+    value: Tuple[str]
+) -> List[Tuple[str, List[Tuple[int, int]]]]:
+    one: str
+    tuples: List[Tuple[str, List[Tuple[int, int]]]] = []
+    cur_gene: str = ''
+    cur_ranges: List[int] = []
     # use $$ as the end, it never goes into the results
-    for one in gene_range_tuples + ('$$', ):
+    for one in value + ('$$', ):
         if one.isdigit():
-            cur_group.append(int(one))
+            cur_ranges.append(int(one))
         else:
-            if cur_group:
-                gene, *ranges = cur_group
-                if len(ranges) % 2 != 0:
+            if cur_gene and cur_ranges:
+                if len(cur_ranges) % 2 != 0:
                     raise click.ClickException(
-                        'Missing paired range values: <GENE>{}'.format(gene)
+                        'Missing paired range values: <GENE>{}'
+                        .format(cur_gene)
                     )
-                ranges = tuple(chunked(ranges, 2))
-                for refstart, refend in ranges:
+                cur_range_chunks: List[Tuple[int, int]] = []
+
+                for refstart, refend in chunked(cur_ranges, 2):
                     if refstart < 1:
                         raise click.ClickException(
                             'argument <REF_START>:{} must be not less than 1'
@@ -38,9 +48,56 @@ def group_gene_range_tuples(gene_range_tuples):
                             ':{} and <REF_END>:{}'
                             .format(refstart, refend)
                         )
-                tuples.append((gene, ranges))
-            cur_group = [one]
+                    cur_range_chunks.append((refstart, refend))
+                tuples.append((cur_gene, cur_range_chunks))
+            cur_gene = one
+            cur_ranges = []
     return tuples
+
+
+class CodonPair(TypedDict):
+    Position: int
+    RefCodonText: str
+    CodonText: str
+    RefAminoAcidText: str
+    AminoAcidText: str
+    InsertedCodonsText: str
+    IsInsertion: bool
+    IsDeletion: bool
+
+
+class AlignedSite(TypedDict):
+    PosAA: int
+    PosNAs: List[Optional[int]]
+    LengthNA: int
+
+
+# XXX: remove total=False after upgraded to 3.11 (PEP 655)
+class FrameShift(TypedDict, total=False):
+    Position: int
+    GapLength: int
+    NucleicAcidsText: Optional[str]
+    IsInsertion: int
+
+
+class GeneReportInner(TypedDict, total=False):
+    FirstAA: int
+    LastAA: int
+    AlignedSites: List[AlignedSite]
+    Mutations: List[CodonPair]
+    FrameShifts: List[FrameShift]
+
+
+class GeneReport(TypedDict):
+    Gene: str
+    Report: GeneReportInner
+    Error: str
+
+
+class Payload(TypedDict):
+    Name: str
+    Modifiers: List[List[str]]
+    GeneReports: List[GeneReport]
 
 
 @cli.command('save-json')
@@ -51,9 +108,13 @@ def group_gene_range_tuples(gene_range_tuples):
 )
 @click.argument(
     'gene_range_tuples',
+    callback=gene_range_tuples_callback,
     nargs=-1
 )
-def save_json(trim_by_seq, gene_range_tuples):
+def save_json(
+    trim_by_seq: bool,
+    gene_range_tuples: List[Tuple[str, List[Tuple[int, int]]]]
+) -> Processor[Iterable[str]]:
     """Save as NucAmino style JSON reports
 
     <GENE_RANGE_TUPLES>:
@@ -61,19 +122,23 @@ def save_json(trim_by_seq, gene_range_tuples):
         <GENE> <REF_START1> <REF_END1> <REF_START2> <REF_END2> ...
         Use to calculate codon position within the protein/gene.
     """
-    num_indent = 2
-    gene_range_tuples = group_gene_range_tuples(gene_range_tuples)
+    num_indent: int = 2
 
-    def processor(iterator):
+    @output_processor('save-json')
+    def processor(iterator: Iterable[RefSeqPair]) -> Iterable[str]:
         # TODO: MSA remap?
+        refseq: Sequence
+        seq: Sequence
         yield '[\n'
         for idx, (refseq, seq) in enumerate(iterator):
-            reftext = refseq.seqtext
-            seqtext = seq.seqtext
+            reftext: NAPosition = refseq.seqtext
+            seqtext: NAPosition = seq.seqtext
             # refstart = None
             # refend = None
             if trim_by_seq:
-                slicekey = find_trim_slice(seq)
+                start: int
+                end: int
+                slicekey: slice = find_trim_slice(seq)
                 start, end, _ = slicekey.indices(len(seq))
                 seqtext[:start].set_flag('trim_by_seq')
                 seqtext[end:].set_flag('trim_by_seq')
@@ -87,45 +152,65 @@ def save_json(trim_by_seq, gene_range_tuples):
                 # if refend == 0:
                 #     refend = None
 
-            payload = {
+            payload: Payload = {
                 'Name': seq.headerdesc,
                 'Modifiers': [[str(mod) for mod in mods]
                               for _, mods in seq.modifiers],
                 'GeneReports': []
             }
 
-            gene_codons = group_by_gene_codons(
+            gene_codons: List[
+                Tuple[
+                    str,
+                    List[List[NAPosition]],
+                    List[List[NAPosition]]
+                ]
+            ] = group_by_gene_codons(
                 reftext, seqtext, gene_range_tuples)
 
+            gene: str
+            refcodons: List[List[NAPosition]]
+            seqcodons: List[List[NAPosition]]
             for gene, refcodons, seqcodons in gene_codons:
-                codonpairs = []
-                aligned_sites = []
-                frameshifts = []
+                pos0: int
+                refcd: List[NAPosition]
+                seqcd: List[NAPosition]
+                codonpairs: List[CodonPair] = []
+                aligned_sites: List[AlignedSite] = []
+                frameshifts: List[FrameShift] = []
                 for pos0, (refcd, seqcd) in enumerate(zip(refcodons,
                                                           seqcodons)):
+                    na: NAPosition
+                    ins_fs_len: int
+                    del_fs_len: int
                     if len(refcd) < 3:
                         # partial matched reference, skip
                         continue
                     # pos0 = (min(na.min_pos for na in refcd
                     #             if na.min_pos) - refstart) // 3
-                    nalen = sum(not na.is_gap() for na in seqcd)
+                    nalen: int = sum(not na.is_gap() for na in seqcd)
                     if 0 < nalen < 3:
                         ins_fs_len = 0
                         del_fs_len = 3 - nalen
                     else:
                         ins_fs_len = nalen % 3
                         del_fs_len = 0
-                    codon_text = ''.join(str(na) for na in seqcd[:3])
+                    # XXX add function to join List[NAPosition]
+                    codon_text: str = ''.join(
+                        str(bytes(na), 'ASCII') for na in seqcd[:3])
                     if any(na.any_has_flag('trim_by_seq') for na in seqcd[:3]):
                         continue
                     codonpairs.append({
                         'Position': pos0 + 1,
                         'RefCodonText': ''.join(str(na) for na in refcd[:3]),
                         'CodonText': codon_text,
-                        'RefAminoAcidText': translate_codon(refcd[:3]),
-                        'AminoAcidText': translate_codon(seqcd[:3]),
+                        'RefAminoAcidText': str(
+                            translate_codon(refcd[:3]), 'ASCII'),
+                        'AminoAcidText': str(
+                            translate_codon(seqcd[:3]), 'ASCII'),
                         'InsertedCodonsText': ''.join(
-                            str(na) for na in seqcd[3:len(seqcd) - ins_fs_len]
+                            str(bytes(na), 'ASCII')
+                            for na in seqcd[3:len(seqcd) - ins_fs_len]
                         ),
                         'IsInsertion': len(refcd) > 5,
                         'IsDeletion': codon_text == '---'
@@ -140,7 +225,8 @@ def save_json(trim_by_seq, gene_range_tuples):
                             'Position': pos0 + 1,
                             'GapLength': ins_fs_len,
                             'NucleicAcidsText': ''.join(
-                                str(na) for na in seqcd[-ins_fs_len:]
+                                str(bytes(na), 'ASCII')
+                                for na in seqcd[-ins_fs_len:]
                             ),
                             'IsInsertion': True
                         })
@@ -150,7 +236,7 @@ def save_json(trim_by_seq, gene_range_tuples):
                             'GapLength': del_fs_len,
                             'IsInsertion': False
                         })
-                mutations = [
+                mutations: List[CodonPair] = [
                     cd for cd in codonpairs
                     if cd['RefAminoAcidText'] != cd['AminoAcidText'] or
                     cd['IsInsertion']
@@ -175,12 +261,10 @@ def save_json(trim_by_seq, gene_range_tuples):
                         'Error': 'Sequence is not aligned'
                     })
 
-            text = json.dumps(payload, indent=num_indent)
+            text: str = json.dumps(payload, indent=num_indent)
             if idx > 0:
                 yield ',\n'
             yield indent(text, prefix=' ' * num_indent)
         yield '\n]\n'
 
-    processor.command_name = 'save-json'
-    processor.is_output_command = True
     return processor
